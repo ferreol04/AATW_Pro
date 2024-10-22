c1691187e6e6285fccdccfac20635857607d9ab1
Nehm12 <houngajunior72@gmail.com>
2024-10-23 00:26:25 +0100

    Login ad register form add

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+  return (
+    <section className="bg-[#F4F7FF] py-20 lg:py-[120px]">
+      <div className="container mx-auto">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4">
+            <div
+              className="
+                relative
+                mx-auto
+                max-w-[525px]
+                overflow-hidden
+                rounded-lg
+                bg-white
+                py-16
+                px-10
+                text-center
+                sm:px-12
+                md:px-[60px]
+              "
+            >
+              <div className="mb-10 text-center md:mb-16">Laraveller</div>
+              <form onSubmit={(e) => { e.preventDefault(); authStore.handleRegister(form); }}>
+                <div className="mb-6">
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    v-model="form.name"
+                    className="
+                      border-[#E9EDF4]
+                      w-full
+                      rounded-md
+                      border
+                      bg-[#FCFDFE]
+                      py-3
+                      px-5
+                      text-base text-body-color
+                      placeholder-[#ACB6BE]
+                      outline-none
+                      focus:border-primary
+                      focus-visible:shadow-none
+                    "
+                  />
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    v-model="form.email"
+                    className="
+                      border-[#E9EDF4]
+                      w-full
+                      rounded-md
+                      border
+                      bg-[#FCFDFE]
+                      py-3
+                      px-5
+                      text-base text-body-color
+                      placeholder-[#ACB6BE]
+                      outline-none
+                      focus:border-primary
+                      focus-visible:shadow-none
+                    "
+                  />
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    v-model="form.password"
+                    className="
+                      border-[#E9EDF4]
+                      w-full
+                      rounded-md
+                      border
+                      bg-[#FCFDFE]
+                      py-3
+                      px-5
+                      text-base text-body-color
+                      placeholder-[#ACB6BE]
+                      outline-none
+                      focus:border-primary
+                      focus-visible:shadow-none
+                    "
+                  />
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="password"
+                    placeholder="Password Confirmation"
+                    v-model="form.password_confirmation"
+                    className="
+                      border-[#E9EDF4]
+                      w-full
+                      rounded-md
+                      border
+                      bg-[#FCFDFE]
+                      py-3
+                      px-5
+                      text-base text-body-color
+                      placeholder-[#ACB6BE]
+                      outline-none
+                      focus:border-primary
+                      focus-visible:shadow-none
+                    "
+                  />
+                </div>
+                <div className="mb-10">
+                  <button
+                    type="submit"
+                    className="
+                      w-full
+                      px-4
+                      py-3
+                      bg-indigo-500
+                      hover:bg-indigo-700
+                      rounded-md
+                      text-white
+                    "
+                  >
+                    Register
+                  </button>
+                </div>
+              </form>
+              <p className="text-base text-[#adadad]">
+                <Link to="/login" className="text-primary hover:underline">
+                  Sign In
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
